38b70b9e043b874953d76a806163e35b3c869e21
lewismartineza <68352284+lewismartineza@users.noreply.github.com>
2024-12-16 11:44:23 -0500

    refactor: change go detail button for link

--- components/character.tsx
+++ components/character.tsx
@@ -1,33 +1,33 @@
-"use client"
-
 import { Character } from "@/types"
-import { Card, Image, CardFooter, Button } from "@nextui-org/react"
-import { useRouter } from "next/navigation";
-
+import Link from "next/link";
+import { Card, Image, CardFooter } from "@nextui-org/react"
 
 type CharacterCardProps = Pick<Character, 'id' | 'image' | 'name' | 'species' | 'status'>
 
-export function CharacterCard(character: CharacterCardProps) {
-    const router = useRouter()
+const STATUS: Record<Character['status'], string> = {
+    'Alive': '🟢 Alive',
+    'Dead': '🔴 Dead',
+    'unknown': '❓ Unknown'
+}
 
+export function CharacterCard(character: CharacterCardProps) {
     return (
-        <Card isFooterBlurred className="w-full h-[300px]">
-            <Image
-                removeWrapper
-                alt="Card example background"
-                className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-                src={character.image}
-            />
-            <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
-                <div>
-                    <p className="text-[purple] font-bold text-md">{character.name}</p>
-                    <p className="text-black text-tiny">{character.species}</p>
-                    <p className="text-black text-tiny">{character.status === 'Alive' ? '🟢 Alive' : character.status === 'Dead' ? '🔴 Dead' : '❓ Unknown'}</p>
-                </div>
-                <Button className="text-tiny" color="primary" radius="full" size="sm" onClick={() => router.push(`${character.id}`)}>
-                    More
-                </Button>
-            </CardFooter>
-        </Card>
+        <Link href={`/${character.id}`}>
+            <Card isFooterBlurred className="w-full h-[300px]">
+                <Image
+                    removeWrapper
+                    alt="Card example background"
+                    className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+                    src={character.image}
+                />
+                <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+                    <div>
+                        <p className="text-[purple] font-bold text-md">{character.name}</p>
+                        <p className="text-black text-tiny">{character.species}</p>
+                        <p className="text-black text-tiny">{STATUS[character.status]}</p>
+                    </div>
+                </CardFooter>
+            </Card>
+        </Link>
     )
 }
