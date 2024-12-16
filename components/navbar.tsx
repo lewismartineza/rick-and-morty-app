3ae18024baac1fbd9a01a1eb83166a3c76a683d0
lewismartineza <68352284+lewismartineza@users.noreply.github.com>
2024-12-16 15:01:50 -0500

    feat:P remove rick and morty image logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -62,7 +62,7 @@ export const Navbar = () => {
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1 py-5" href="/">
-            <Logo />
+            {/* <Logo /> */}
             <p className="font-bold text-inherit">Rick & Morty</p>
           </NextLink>
         </NavbarBrand>
