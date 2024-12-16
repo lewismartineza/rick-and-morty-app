9ae31304fa870f1f28e2d5c49ef9dac9e0fb484a
Lewis <lmartinez@liveventures.com>
2024-12-15 21:52:41 -0500

    feat: create character card

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+"use client"
+import { CharacterCard } from "@/components/character";
 import { title } from "@/components/primitives";
 
 export default function Home() {
+
+  const character = {
+    id: 1,
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    name: 'Rick Sanchez',
+    status: 'Alive',
+    species: 'Human'
+  }
+
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
@@ -9,8 +20,8 @@ export default function Home() {
         <span className={title()}>Morty&nbsp;</span>
       </div>
 
-      <div className="mt-8">
-        Hello World
+      <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {Array(10).fill(0).map((_, i) => <CharacterCard key={character.name + i}  {...character} />)}
       </div>
     </section>
   );
---
+++ components/character.tsx
@@ -0,0 +1,27 @@
+import { Character } from "@/types"
+import { Card, Image, CardFooter, Button } from "@nextui-org/react"
+
+type CharacterCardProps = Pick<Character, 'id' | 'image' | 'name' | 'species' | 'status'>
+
+export function CharacterCard(character: CharacterCardProps) {
+    return (
+        <Card isFooterBlurred className="w-full h-[300px]">
+            <Image
+                removeWrapper
+                alt="Card example background"
+                className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+                src={character.image}
+            />
+            <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+                <div>
+                    <p className="text-[purple] font-bold text-md">{character.name}</p>
+                    <p className="text-black text-tiny">{character.species}</p>
+                    <p className="text-black text-tiny">{character.status === 'Alive' ? '🟢 Alive' : character.status === 'Dead' ? '🔴 Dead' : '❓ Unknown'}</p>
+                </div>
+                <Button className="text-tiny" color="primary" radius="full" size="sm">
+                    Notify Me
+                </Button>
+            </CardFooter>
+        </Card>
+    )
+}
