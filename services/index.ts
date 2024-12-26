4a0d6d82c0eef9973cd466c5a61836ccc2217ce2
Lewis <lmartinez@liveventures.com>
2024-12-25 20:29:14 -0500

    fix: update environment variable to use NEXT_PUBLIC_API_URL prefix

--- services/index.ts
+++ services/index.ts
@@ -1,18 +1,18 @@
 import { Character, Info } from "@/types";
 
-type CharacterResult = {
+export type CharacterResult = {
   info: Info,
   results: Character[]
 }
 
 export async function getCharacters(page = 1, search = ""): Promise<CharacterResult> {
-  const response = await fetch(`${process.env.API_URL}/character/?page=${page}&name=${search}`)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/character/?page=${page}&name=${search}`)
   const data = await response.json();
   return data
 }
 
 export async function getCharacterDetail(id: string): Promise<Character> {
-	const response = await fetch(`${process.env.API_URL}/character/${id}`);
+	const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/character/${id}`);
 	const data = await response.json();
 	return data;
 }
