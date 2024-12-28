1ea76f94bbed407b8003391b8152e926615b1d5e
lewismartineza <68352284+lewismartineza@users.noreply.github.com>
2024-12-28 01:29:48 -0500

    feat: add motion for animations

--- components/character-detail.tsx
+++ components/character-detail.tsx
@@ -2,6 +2,9 @@
 
 import "./character-detail.scss";
 
+import * as motion from "motion/react-client";
+import { AnimatePresence } from "motion/react";
+
 import { useGetCharacterDetail } from "@/hooks";
 import { Character } from "@/types";
 import { Image } from "@nextui-org/react";
@@ -19,73 +22,88 @@ export function CharacterDetail({ character }: CharacterDetailProps) {
     if (isError) return <div>Error to load character.</div>
 
     return (
-        <div className="character-detail-container">
-            <div className="md:flex">
-                <div className="md:flex-shrink-0 flex justify-center">
-                    <Image
-                        src={data?.image}
-                        alt={data?.name}
-                        width={300}
-                        height={300}
-                        className="banner--image"
-                    />
-                </div>
-                <div className="p-8 text-center lg:w-full md:w-96">
-                    <div className="uppercase tracking-wide text-sm text-green-400 font-semibold mb-1">
-                        {data?.species} - {data?.status}
+        <AnimatePresence mode="wait">
+            <motion.div
+                initial={{ y: 10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -10, opacity: 0 }}
+                transition={{ duration: 0.4 }}
+            >
+                <div className="character-detail-container">
+                    <div className="md:flex">
+                        <div className="md:flex-shrink-0 flex justify-center">
+                            <Image
+                                src={data?.image}
+                                alt={data?.name}
+                                width={300}
+                                height={300}
+                                className="banner--image"
+                            />
+                        </div>
+                        <div className="p-8 text-center lg:w-full md:w-96">
+                            <div className="uppercase tracking-wide text-sm text-green-400 font-semibold mb-1">
+                                {data?.species} - {data?.status}
+                            </div>
+                            <h1 className="text-4xl font-bold text-white leading-tight mb-2">
+                                {data?.name}
+                            </h1>
+                            <p className="text-gray-300 mb-4">
+                                {data?.type ? `Type : ${data?.type}` : 'No specific type'}
+                            </p>
+                            <div className="grid grid-cols-2 gap-4 text-sm">
+                                <div>
+                                    <span className="text-green">Gender:</span> {data?.gender}
+                                </div>
+                                <div>
+                                    <span className="text-green origin">Origin:</span> {data?.origin.name}
+                                </div>
+                                <div>
+                                    <span className="text-green">Location:</span> {data?.location.name}
+                                </div>
+                                <div>
+                                    <span className="text-green">Episodes:</span> {data?.episode.length}
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <h1 className="text-4xl font-bold text-white leading-tight mb-2">
-                        {data?.name}
-                    </h1>
-                    <p className="text-gray-300 mb-4">
-                        {data?.type ? `Type : ${data?.type}` : 'No specific type'}
-                    </p>
-                    <div className="grid grid-cols-2 gap-4 text-sm">
-                        <div>
-                            <span className="text-green">Gender:</span> {data?.gender}
-                        </div>
-                        <div>
-                            <span className="text-green origin">Origin:</span> {data?.origin.name}
-                        </div>
-                        <div>
-                            <span className="text-green">Location:</span> {data?.location.name}
-                        </div>
-                        <div>
-                            <span className="text-green">Episodes:</span> {data?.episode.length}
+
+                    <div className="episodes">
+                        <h2 className="episodes--title">Episode Appearances</h2>
+                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+                            {data?.episode.map((ep) => (
+                                <motion.div
+                                    key={ep}
+                                    whileHover={{ scale: 1.05 }}
+                                    whileTap={{ scale: 0.8 }}
+                                >
+                                    <div className="bg-purple-800/50 p-2 rounded text-center text-sm episode" data-testid="episode">
+                                        Episode {ep.split('/').pop()}
+                                    </div>
+                                </motion.div>
+                            ))}
                         </div>
                     </div>
-                </div>
-            </div>
 
-            <div className="episodes">
-                <h2 className="episodes--title">Episode Appearances</h2>
-                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-                    {data?.episode.map((ep) => (
-                        <div key={ep} className="episodes--item" data-testid="episode">
-                            Episode {ep.split('/').pop()}
+                    <div className="character-timeline">
+                        <h2 className="character-timeline--title">Character Timeline</h2>
+                        <div className="relative">
+                            <div className="absolute h-full w-1 bg-green left-1/2 transform -translate-x-1/2" />
+                            <div className="flex flex-col space-y-8">
+                                <TimelineItem title="First Appearance" content={`Episode ${data?.episode[0].split('/').pop()}`} />
+                                <TimelineItem title="Origin" content={data?.origin.name ?? ""} />
+                                <TimelineItem title="Current Location" content={data?.location.name ?? ""} />
+                                <TimelineItem title="Last Known Appearance" content={`Episode ${data?.episode[character.episode.length - 1].split('/').pop()}`} />
+                            </div>
                         </div>
-                    ))}
-                </div>
-            </div>
+                    </div>
 
-            <div className="character-timeline">
-                <h2 className="character-timeline--title">Character Timeline</h2>
-                <div className="relative">
-                    <div className="absolute h-full w-1 bg-green left-1/2 transform -translate-x-1/2" />
-                    <div className="flex flex-col space-y-8">
-                        <TimelineItem title="First Appearance" content={`Episode ${data?.episode[0].split('/').pop()}`} />
-                        <TimelineItem title="Origin" content={data?.origin.name ?? ""} />
-                        <TimelineItem title="Current Location" content={data?.location.name ?? ""} />
-                        <TimelineItem title="Last Known Appearance" content={`Episode ${data?.episode[character.episode.length - 1].split('/').pop()}`} />
+                    <div className="date-added">
+                        <p className="text-sm text-gray-400">
+                            Character added to database on: {new Date(data?.created ?? "").toLocaleDateString()}
+                        </p>
                     </div>
                 </div>
-            </div>
-
-            <div className="date-added">
-                <p className="text-sm text-gray-400">
-                    Character added to database on: {new Date(data?.created ?? "").toLocaleDateString()}
-                </p>
-            </div>
-        </div>
+            </motion.div>
+        </AnimatePresence>
     )
 }
